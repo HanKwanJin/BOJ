2eda2cc2cc419a2bf074eb1ed9d4b2394cc0b430
kwanjin <rhkswls98@naver.com>
2021-11-22 16:35:27 +0900

    BOJ 11656

---
+++ 11656.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    string tmp[1001];
+    cin >> s;
+    int d = s.size();
+    for(int i=0; i< d; i++){
+        for(int j =i; j< d; j++){
+            tmp[i] += s[j];
+        }
+    }
+
+    sort(tmp, tmp+d);
+    for(int i=0; i< d; i++){
+        cout << tmp[i] << '\n';
+    }
+}
