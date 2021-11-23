d168ee3b9150113c247eca4d9e3998b053d84281
DoooongDong <49313910+HanKwanJin@users.noreply.github.com>
2021-11-23 18:51:51 +0900

    승부 조작

--- 23353.cpp
+++ 23353.cpp
@@ -17,7 +17,7 @@ int main(void){
         for(int j=0; j<n; j++){
             cin >> board[i][j];
         }
-    }
+    } 
     //검은 돌 끼리만 있을 때
     for(int i = n-1; i >= 0; i--){
         for(int j=n-1; j >= 0; j--){
