7702adaa2197f7aff2792ff7da116da2e594bd8e
kwanjin <rhkswls98@naver.com>
2021-11-23 19:01:40 +0900

    역원소 정렬

---
+++ 정렬/5648.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+     
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    vector<string> v(n);
+    vector<ll> a;
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+        reverse(v[i].begin(), v[i].end());
+        a.push_back(stoll(v[i]));
+    }
+
+    sort(a.begin(), a.end());
+
+    for(int i=0; i<n; i++){
+        cout << a[i] << '\n';
+    }
+
+
+}
