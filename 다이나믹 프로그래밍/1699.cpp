ab9b3a99beda2786b8446acb61170a6e1ff385ee
DoooongDong <rhkswls98@kakao.com>
2022-01-01 22:23:02 +0900

    제곱수의 합

---
+++ 다이나믹 프로그래밍/1699.cpp
@@ -0,0 +1,17 @@
+//제곱수의 합
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int d[100001];
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for(int i=1; i<= n; i++){
+        d[i] = i;
+        for(int j=1; j*j <= i; j++){
+            d[i] = min(d[i], d[i-j*j] + 1);
+        }
+    }
+    cout << d[n];
+}
