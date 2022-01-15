00869db6af106b9caf0127c055e544e9f48de9f5
DoooongDong <rhkswls98@kakao.com>
2022-01-16 02:27:43 +0900

    랜선 자르기

--- 이분 탐색/1654.cpp
+++ 이분 탐색/1654.cpp
@@ -1,45 +1,24 @@
+//랜선 자르기
 #include<bits/stdc++.h>
-typedef long long ll;
-using namespace std;
+using namespace std; 
 
-ll n,k; //k 랜선개수, n 필요한 랜선 개수
-ll arr[10001];
+int k,n;
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin >> k >> n;
-    for(ll i=0; i<k; i++){
-        cin >> arr[i];
+    vector<int> v(k);
+    for(int & i: v) cin >> i;
+
+    auto f = [&](int x){
+        int s =0;
+        for(int i: v) if(i >= x) s += i / x;
+        return s >= n;
+    };
+    int st = 1;
+    int en = 1e6;
+    while(en /= 2){
+        while(f(st + en)) st += en;
     }
-    sort(arr,arr+k);
-    ll left = 1;
-    ll right = arr[k-1];
-    ll mid =0;
-    ll cnt=0;
-
-
-    if(n == 1){
-        cout << arr[0];
-        return 0;
-    }
-
-    while(left <= right || cnt < n){
-        mid = (left + right) / 2;
-        cnt =0;
-
-        for(ll i=0; i<k; i++){
-            cnt += arr[i] / mid;
-        }
-
-        if(cnt >= n){
-            left = mid + 1;
-        }else if(cnt < n){
-            right = mid - 1;
-        }
-
-        
-        
-    }
-    cout << mid << '\n';
+    cout << st;
 }
