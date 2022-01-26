892ba1a988a544b1ac4788bba4bb713a689cc6ca
DoooongDong <rhkswls98@kakao.com>
2022-01-26 14:42:01 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ 수학/18108.cpp
@@ -0,0 +1,12 @@
+//1998년생인 내가 태국에서는 2541년생?!
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a;
+    cin >> a;
+    a -= 543;
+    cout << a;
+}
