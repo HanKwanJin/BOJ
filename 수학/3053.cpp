059bce2af6e65dfff229b2abb015e0f55b8f43fd
DoooongDong <rhkswls98@kakao.com>
2022-02-08 21:39:31 +0900

    택시 기하학

---
+++ 수학/3053.cpp
@@ -0,0 +1,15 @@
+//택시 기하학
+#include<bits/stdc++.h>
+#define PI 3.1415926535897932
+using namespace std;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    double r;
+    cin >> r;
+    cout << fixed;
+    cout.precision(6);
+    
+    cout << r * r * PI << '\n';
+    cout << 2 * r * r << '\n';
+}
