f772dc083d960defc762fddaa65598a430f2718c
DoooongDong <rhkswls98@kakao.com>
2023-01-03 22:19:32 +0900

    [BOJ] 1285 동전 뒤집기

--- 비트마스킹/1285.cpp
+++ 비트마스킹/1285.cpp
@@ -1,41 +1,50 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 #include<string>
-#include<algorithm>
+#include<cstring>
 using namespace std;
-const int INF = 987654321;
-int n, a[44],ret = INF;
+const int INF = 1e9;
+int n;
+int ret = INF;
+int a[24];
 string s;
-
-void go(int cur){
-	if(cur == n+1){
+int cc;
+void func(int cur){
+	if(cur == n + 1){	
 		int sum = 0;
-		for(int i=1; i<= (1 << (n - 1)); i *= 2){
+		for(int i=1; i<= 1 << (n-1); i*=2){
 			int cnt = 0;
-			for(int j=1; j<=n; j++) if(a[j] & i) cnt++;
+			for(int j=1; j<=n; j++){
+				cc++;
+				if(a[j] & i) cnt++;
+			}
 			sum += min(cnt, n - cnt);
 		}
 		ret = min(ret, sum);
 		return;
 	}
+	
 	a[cur] = ~a[cur];
-	go(cur+1);
+	func(cur + 1);
 	a[cur] = ~a[cur];
-	go(cur+1);
+	func(cur + 1);
 }
+
 int main(void){
-	ios::sync_with_stdio(0);
+	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	cin >> n;
 	for(int i=1; i<=n; i++){
 		cin >> s;
-		int value = 1;
+		int v = 1;
 		for(int j=0; j<s.size(); j++){
-			if(s[j] == 'T') a[i] |= value;
-			value *= 2;
+			if(s[j] == 'T') a[i] |= v;
+			v *= 2;
 		}
 	}
-	go(1);
+	func(1);
+	cout << cc << " ----\n";
 	cout << ret;
 	return 0;
 }
