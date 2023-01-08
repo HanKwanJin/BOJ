99e580d7200f62e7adabddd9d6570b956d76ae3f
DoooongDong <rhkswls98@kakao.com>
2023-01-08 16:08:18 +0900

    [BOJ] 2293 동전 1

--- 다이나믹 프로그래밍/2293.cpp
+++ 다이나믹 프로그래밍/2293.cpp
@@ -1,22 +1,24 @@
-//동전1
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
 using namespace std;
-typedef long long ll;
 int n,k;
-ll d[10001]; //d[a] = b a원을 만들기 위한 경우의 수가 b임
-int coin[101];
+int coin[104];
+int dp[10004];
+
 int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> k;
-    for(int i=1; i<=n; i++){
-        cin >> coin[i];
-    }
-    d[0] = 1; //아무것도 선택 안했을때 0을 만드는 경우의 수는 1이다
-    for(int i=1; i<= n; i++){
-        for(int j=coin[i]; j<= k; j++){
-            d[j] = d[j] + d[j-coin[i]]; 
-        }
-    }
-    cout << d[k];
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> n >> k;
+	for(int i=1; i<=n; i++){
+		cin >> coin[i];
+	}
+	dp[0] = 1;
+	for(int i=1; i<=n; i++){
+		if(coin[i] >= 10001) continue;
+		for(int j=coin[i]; j<=k; j++){
+			dp[j] += dp[j-coin[i]];
+		}
+	}
+	cout << dp[k];
+	return 0;
 }
